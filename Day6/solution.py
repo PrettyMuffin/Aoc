3decdb46ba9507419399e64ad8c043c94ba4786b
Marco Brunello <markobrunello2010@gmail.com>
2025-12-13 14:03:06 +0100

    day6

--- Day6/solution.py
+++ Day6/solution.py
@@ -1,4 +1,7 @@
 import os
+from math import prod
+from collections import deque
+import itertools
 
 operators = {"*", "+"}
 results = []
@@ -16,43 +19,143 @@ def last_line(file):
 
 
 def operate(total: int, value: int, operator: str):
-    if operator not in operators:
-        raise ValueError(f"Operatore {operator} non valido")
-    if operator == "+":
-        total += value
-    else:
-        total *= value
-    return total
+    return total + value if operator == "+" else total * value
 
 def parse_operators(line):
-    result = []
-    for el in line:
-        if el in operators:
-            result.append(el)
-    return result
+    return [(i, c) for i, c in enumerate(line) if c in operators]
+
+def print_matrix(matrix):
+    for line in matrix:
+        print(line)
 
 def part1():
-    with open("./Day6/input.txt", "rb") as file:
-        parsed_operators = parse_operators(last_line(file))
-        for operator in parsed_operators:
-            if operator == "+":
-                results.append(0)
-            else:
-                results.append(1)
-        file.seek(0)
-        element_read = file.read(4).decode().strip() # leggo un intero
+    with open("./Day6/input.txt", "r") as file:
+        lines = file.read().splitlines()
+        parsed_operators = parse_operators(lines.pop())
+        results = [0 if op == "+" else 1 for op in parsed_operators]
         i = 0
-        while element_read:
-            if element_read in operators: # letto operatore, basta
-                break
-            results[i] = operate(results[i], int(element_read), parsed_operators[i])
-            if i == len(results) - 1:
-                i = 0
-            else:
-                i += 1
-            element_read = file.read(4).decode().strip() # leggo un intero
+        for line in lines:
+            for num in line.split():
+                if num:
+                    results[i] = operate(results[i], int(num), parsed_operators[i])
+                    i = (i + 1) % len(results)
+
         print(sum(results))
 
 
-part1()
 
+def part1_pythonica():
+    with open("./Day6/test_input.txt", "r") as file:
+        lines = file.read().splitlines()
+        parsed_operators = parse_operators(lines.pop())
+        op_indx = [i for i, _ in parsed_operators]
+        op_indx.append(len(lines[0]))
+        slices = [(start, end) for start, end in itertools.pairwise(op_indx)]
+        matrix = [
+            [
+                int(line[start: end].replace(" ", "")) 
+                for start, end in slices
+                
+            ]
+            for line in lines
+        ]
+        columns = zip(*matrix)
+        tots = [
+            sum(col) if op == "+" else prod(col)
+            for col, (_, op) in zip(columns, parsed_operators) 
+        ]
+        print(sum(tots))
+
+
+
+def part2():
+    file = open("./Day6/input.txt", "r")
+    lines = file.read().splitlines()
+    parsed_operators = parse_operators(lines.pop())
+    file.close()
+
+    matrix = []
+
+    for line in lines:
+        i = 1
+        prev_indx = parsed_operators[0][0]
+        j = 0
+        matrix_row = []
+        while j < len(line):
+            if i < len(parsed_operators):
+                indx = parsed_operators[i][0]
+            else:
+                indx = len(line)
+            matrix_row.append(int(line[prev_indx: indx - 1 if indx < len(line) else indx].replace(" ", "0")))
+            j += indx - prev_indx
+            i += 1
+            prev_indx = indx
+        matrix.append(matrix_row)
+
+
+    results = [0 if op[1] == "+" else 1 for op in parsed_operators]
+    div = 10
+
+    for col in range(len(matrix[0])):
+        zero_counter = 0
+        while zero_counter < len(matrix):
+            value = ""
+            for row in range(len(matrix)):
+                if matrix[row][col] == 0:
+                    continue
+
+                value += str(matrix[row][col] % div) if matrix[row][col] % div != 0 else ""
+                matrix[row][col] //= div
+                if matrix[row][col] == 0:
+                    zero_counter += 1
+            results[col] = operate(results[col], int(value), parsed_operators[col][1])
+
+    return sum(results)
+
+def part2_pythonica():
+    file = open("./Day6/input.txt", "r")
+    lines = file.read().splitlines()
+    parsed_operators = parse_operators(lines.pop())
+    file.close()
+
+    indx = [i for i, _ in parsed_operators]
+    indx.append(len(lines[0]))
+    matrix = [
+        [
+            line[indx[i-1]: el - 1 if i < len(indx) - 1 else el].replace(" ", "0")
+            for i, el in enumerate(indx)
+            if i != 0
+        ] 
+        for line in lines
+    ]
+
+    from math import prod
+
+    columns = list(zip(*matrix))
+    tot_values = deque()
+    def evaluate(lista: list[int], op):
+        return sum(lista) if op == "+" else prod(lista)
+    # for i, col in enumerate(columns):
+    #     numeri_verticali = list(zip(*col)) # stampa i numeri in maniera vertiale
+    #     numbers = [int("".join(el).replace("0",'')) for el in numeri_verticali]
+    #     tot_values[i] = evaluate(numbers, parsed_operators[i][1])
+    for col, (_, op) in zip(columns, parsed_operators):
+        numeri_verticali = list(zip(*col)) # stampa i numeri in maniera vertiale
+        numbers = [int("".join(el).replace("0",'')) for el in numeri_verticali]
+        tot_values.append(evaluate(numbers, op))
+    return sum(tot_values)
+
+# part1()
+
+import time
+start = time.perf_counter()
+sol1 = part2()
+end = time.perf_counter()
+print("La soluzione in stile C è: ", end - start)
+
+start = time.perf_counter()
+sol2 = part2_pythonica()
+end = time.perf_counter()
+print("La soluzione pythonica è ", end-start)
+
+assert(sol1 == sol2)
